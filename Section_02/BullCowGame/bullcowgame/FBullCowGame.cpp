a7d7677bed12a5195ffe9a325d113bafbcdb64c1
jedijamez567 <smith.dakota52@gmail.com>
2019-04-28 11:23:43 -0700

    Added gitignore file, created header file and class for bullcowgame

---
+++ Section_02/BullCowGame/bullcowgame/FBullCowGame.cpp
@@ -0,0 +1,31 @@
+#include "FBullCowGame.h"
+
+void FBullCowGame::Reset()
+{
+	return 0;
+}
+
+int FBullCowGame::GetMaxTries()
+{
+	return 0;
+}
+
+int FBullCowGame::GetCurrentTry()
+{
+	return 0;
+}
+
+bool FBullCowGame::CheckWord(std::string)
+{
+	return false;
+}
+
+std::string FBullCowGame::GenerateWord()
+{
+	return std::string();
+}
+
+bool FBullCowGame::Outcome()
+{
+	return false;
+}
